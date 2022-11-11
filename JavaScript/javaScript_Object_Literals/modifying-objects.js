f66e8a02d2febb91c3250c57f28580edc2480ccd
Olalekan Shogbaike <olalekanshogbaike@gmail.com>
2022-11-11 11:32:52 +0100

    objects

---
+++ JavaScript/javaScript_Object_Literals/modifying-objects.js
@@ -0,0 +1,9 @@
+const studentAge = {
+    Michael: '21',
+    Morgan: '18',
+    Cynthia: '21',
+}
+studentAge.Cynthia = 19; // modifying/changing Cynthia's age
+studentAge['Morgan'] = 17; // modifying/changing Morgan's age
+
+studentAge['Olalekan'] = 24; //to modify by adding/creating a new object
---
+++ JavaScript/javaScript_Object_Literals/nesting-arrays-and-objects.js
@@ -0,0 +1,18 @@
+const teenage = [
+    {
+        username: 'Maria West',
+        age: '36',
+        country: 'South Africa',
+    },
+    {
+        username: 'Bone manis',
+        age: '27',
+        country: 'Nigeria',
+    },
+    {
+        username: 'Will Smith',
+        age: '21',
+        country: 'United State',
+    },
+]
+console.log (teenage);
