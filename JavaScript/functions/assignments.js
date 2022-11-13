98caa4d0b1a90fa48f4c996c5465e91e7202ac2e
lehqseel <olalekanshogbaike@gmail.com>
2022-11-13 20:42:12 +0100

    functioning

--- JavaScript/functions/assignments.js
+++ JavaScript/functions/assignments.js
@@ -1,8 +1,6 @@
 /*Define a function called rant which accepts a string
-argument called message. The function should 
-print out an uppercased version of message 3 
-times (with 3 separate calls to console.log).
-For example, rant("I hate beets")*/
+argument called message. The function should print out an uppercased version of message 3 
+times (with 3 separate calls to console.log). For example, rant("I hate beets")*/
 
 function rant(mesage) {
     for (let i = 1; i <= 3; i++) {
@@ -14,8 +12,7 @@ rant('i hate beets')
 
 /*In some dice games like Craps, a roll  of two 1's is called "Snake Eyes". It's generally
 not a good roll.  Please write a  function called isSnakeEyes, which accepts
-two numbers as inputs, representing two dice. If the two numbers are both 1's, 
-please print "Snake Eyes!" otherwise print "Not Snake Eyes!"
+two numbers as inputs, representing two dice. If the two numbers are both 1's, please print "Snake Eyes!" otherwise print "Not Snake Eyes!"
 
 isSnakeEyes(1,1) //Snake Eyes!
 isSnakeEyes(1,5) //Not Snake Eyes!
